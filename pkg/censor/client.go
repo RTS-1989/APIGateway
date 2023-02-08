deb07421b2c6c1ac39f72d503db8d215bed434dc
RTS-1989 <63114573+RTS-1989@users.noreply.github.com>
2023-02-08 11:15:13 +0500

    Added interceptor for censor svc

--- pkg/censor/client.go
+++ pkg/censor/client.go
@@ -2,6 +2,7 @@ package censor
 
 import (
 	"fmt"
+	"github.com/RTS-1989/go-api-gateway/pkg/censor/middleware"
 	"github.com/RTS-1989/go-api-gateway/pkg/censor/pb"
 	"github.com/RTS-1989/go-api-gateway/pkg/config"
 	"google.golang.org/grpc"
@@ -13,7 +14,7 @@ type ServiceClient struct {
 
 func InitServiceClient(c *config.Config) pb.CensorServiceClient {
 	// using WithInsecure() because no SSL running
-	cc, err := grpc.Dial(c.CensoredSvcUrl, grpc.WithInsecure())
+	cc, err := grpc.Dial(c.CensoredSvcUrl, grpc.WithInsecure(), middleware.WithClientUnaryInterceptor())
 
 	if err != nil {
 		fmt.Println("Could not connect:", err)

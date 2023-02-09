9fe5e1734ad876e86d2f45e989e29bd98f6af283
RTS-1989 <63114573+RTS-1989@users.noreply.github.com>
2023-02-09 16:16:38 +0500

    Added request logging into file

--- pkg/gonews/middleware/interceptor.go
+++ pkg/gonews/middleware/interceptor.go
@@ -8,6 +8,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 	"log"
+	"os"
 	"time"
 )
 
@@ -53,13 +54,27 @@ func clientInterceptor(
 		replyValues := reply.(*pb.DetailedNewsResponse)
 		responseStatus = replyValues.Status
 	}
-	log.Printf("Invoked RPC method=%s; destination=%s; requestId=%s; status=%d; Duration=%s; Error=%v",
+	f, err := os.OpenFile("api-gateway-request.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+
+	logger := log.New(f, "RequestLogger: ", log.LstdFlags)
+
+	logger.Printf("Invoked RPC method=%s; destination=%s; requestId=%s; status=%d; Duration=%s; Error=%v",
 		method,
 		cc.Target(),
 		meta.Get(DefaultXRequestIDKey)[0],
 		responseStatus,
 		time.Since(start),
 		err)
+
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Printf("Got error %v\n", err)
+		}
+	}(f)
 
 	return err
 }
